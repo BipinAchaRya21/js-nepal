a6a001ee221f8f057c6a431cd4bed9b54bd7b79a
BipinAchaRya21 <bipinacharya981@gmail.com>
2025-06-20 17:51:33 +0000

    container config done

---
+++ .devcontainer/01_basic/sec.js
@@ -0,0 +1,10 @@
+const accountId = 1408;
+let accountEmail = "bipin10@gmail.com";
+var accountPassword = "34555";
+let accountCity = "birtamod";
+accountEmail = "aditya0@gmail.com";
+accountCity = "bhadrapur";
+let accountState;
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountState]);
