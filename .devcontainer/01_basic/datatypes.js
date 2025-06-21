d7d7067b4aa4fc07211c7e1b35b22533914244b9
BipinAchaRya21 <bipinacharya981@gmail.com>
2025-06-21 05:28:30 +0000

    datatypes

---
+++ .devcontainer/01_basic/datatypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all js code as new version
+//alert(3+3)//we are using nodejs,not browser
+console.log(3+3)//code readbility should be High
+console.log("Bipin")
+let name="Aditya"
+let age=17
+let isLoggedIn=false//boolean types
+
+//number=>2 to power 53
+//BigInt
+//String=""
+//boolean=>true/false
+//null=>standalone value
+// undefined=>
+    //symbol=>unique
+//object
+console.log(typeof"Bipin");
+console.log(typeof null);
+
